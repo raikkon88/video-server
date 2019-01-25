cc7d51878fee8373490495778f8e1635991f5f22
Marc Sànchez Pifarré <msanxes@gmail.com>
2019-01-25 22:34:12 +0000

    [FEAT] afegida peli del reves per testing

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -9,7 +9,7 @@
   </head>
   <body>
     <video width="320" height="240" controls>
-      <source src="videos/Run_5_wo_metadata_h264420_720p_UHQ.mp4" type="video/mp4">
+      <source src="videos/Del_Reves.mp4" type="video/mp4">
       Your browser does not support the video tag.
     </video>
   </body>
