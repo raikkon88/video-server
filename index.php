5f0c2a43b2fcfa2174771878bc1156f04838d85c
Marc Sànchez <msanxes@gmail.com>
2019-01-25 20:19:13 +0100

    [FEAT] first commit without videos folder

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <video width="320" height="240" controls>
+      <source src="videos/Run_5_wo_metadata_h264420_720p_UHQ.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </body>
+</html>
